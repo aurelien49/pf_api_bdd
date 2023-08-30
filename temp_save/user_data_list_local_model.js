7c05b7ede8a719a84c15ed3a7e3f296b9a8e1d81
Aurelien VAILLANT <aurelienvaillant@outlook.fr>
2023-08-30 23:26:51 +0200

    delete some folders

--- temp_save/user_data_list_local_model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const DataListLocalModel = require('./data_list_local_model');
-
-const userDataListSchema = mongoose.Schema({
-    idUser: { type: Schema.Types.ObjectId, auto: true },
-    userRecords: { type: [DataListLocalModel.schema], required: true },
-});
-
-module.exports = mongoose.model('UserDataListLocalModel', userDataListSchema);
